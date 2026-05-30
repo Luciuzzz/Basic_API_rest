0afcbdff65d2c415e279d5ac3f03baad8d0d96f2
Luis Medina <lm9034064@gmail.com>
2026-05-30 12:29:43 -0300

    Fix

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,10 +10,10 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_the_application_has_no_web_root_route(): void
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertNotFound();
     }
 }
